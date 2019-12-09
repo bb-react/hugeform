c6364a17636185f58a618109f77f470a30e493d3
Boris Brtáň <58065327+bb-react@users.noreply.github.com>
2019-12-09 23:05:43 +0100

    Updates

---
+++ precache-manifest.94847912eb5d84ac72727cb79101c67e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8921963ace72aa6176f9af5b5b3a17a4",
+    "url": "/hugeform/index.html"
+  },
+  {
+    "revision": "5bbcd312e02c759eaa8d",
+    "url": "/hugeform/static/css/main.077f011e.chunk.css"
+  },
+  {
+    "revision": "753851963dab2bb215d7",
+    "url": "/hugeform/static/js/2.4ac9df3f.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "/hugeform/static/js/2.4ac9df3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bbcd312e02c759eaa8d",
+    "url": "/hugeform/static/js/main.0a225096.chunk.js"
+  },
+  {
+    "revision": "d0a31fb8a79876b3584c",
+    "url": "/hugeform/static/js/runtime-main.2a539243.js"
+  }
+]);
